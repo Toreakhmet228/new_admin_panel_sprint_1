796f643be0d24367d2f5f41b4ddfddf7be741127
Toreakhmet228 <113284200+Toreakhmet228@users.noreply.github.com>
2024-07-19 09:22:32 +0500

    Update settings.py

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sqi7$salrc@p1)c=(0!l=w!alvig7m$r1t8tn8i^tk+$g-3+3$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 1
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
